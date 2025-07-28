dc7bb20dc21b8663b37912aa487e7d84b574d386
Mahmoudaboutaleb <112221498+Mahmoudaboutaleb@users.noreply.github.com>
2025-07-29 00:50:16 +0300

    add montserrat font family to our app

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app_with_clean_architecture/features/splash/presentation/
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(DevicePreview(enabled: true, builder: (context) => const BooklyApp()));
@@ -14,7 +15,10 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
       useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
