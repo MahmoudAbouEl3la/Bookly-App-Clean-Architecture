980064ff65dc2e7e6486f376c6a52635356cf4ea
Mahmoudaboutaleb <112221498+Mahmoudaboutaleb@users.noreply.github.com>
2025-08-09 16:22:10 +0300

    register adapter & open featured box

--- lib/constans.dart
+++ lib/constans.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff100b20);
 const kTransitiionDuration = Duration(milliseconds: 350);
 const kGtSectraFine = 'GT Sectra Fine';
+const kFeaturedBox = "featuredBox";
 
 class ImagePath {
   static const String kLogoImage = 'assets/images/Logo.png';
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:bookly_app_with_clean_architecture/constans.dart';
 import 'package:bookly_app_with_clean_architecture/core/utilis/app_router.dart';
+import 'package:bookly_app_with_clean_architecture/features/home/domain/entities/book_entity.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
   runApp(DevicePreview(enabled: true, builder: (context) => BooklyApp()));
 }
 
