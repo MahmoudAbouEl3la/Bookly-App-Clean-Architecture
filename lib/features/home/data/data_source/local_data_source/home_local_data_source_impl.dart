5e082b5fdee136277faab331d97e655ed826a2ab
Mahmoudaboutaleb <112221498+Mahmoudaboutaleb@users.noreply.github.com>
2025-08-09 16:15:29 +0300

    generate book entity adapter

--- lib/features/home/data/data_source/local_data_source/home_local_data_source_impl.dart
+++ lib/features/home/data/data_source/local_data_source/home_local_data_source_impl.dart
@@ -1,10 +1,10 @@
-import 'package:bookly_app_with_clean_architecture/features/home/data/data_source/local_data_source/home_local_data_source.dart';
-import 'package:bookly_app_with_clean_architecture/features/home/domain/entities/book_entity.dart';
+// import 'package:bookly_app_with_clean_architecture/features/home/data/data_source/local_data_source/home_local_data_source.dart';
+// import 'package:bookly_app_with_clean_architecture/features/home/domain/entities/book_entity.dart';
 
-class HomeLocalDataSourceImpl extends HomeLocalDataSource {
-  @override
-  List<BookEntity> fetchFeaturedBooks() {}
+// class HomeLocalDataSourceImpl extends HomeLocalDataSource {
+//   @override
+//   List<BookEntity> fetchFeaturedBooks() {}
 
-  @override
-  List<BookEntity> fetchNewestBooks() {}
-}
+//   @override
+//   List<BookEntity> fetchNewestBooks() {}
+// }
